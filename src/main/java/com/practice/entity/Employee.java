462cfac00f7344f58804d2dd532b2a74c5e6e9d9
Subhrajyoti1997 <subhrajyotipanda1997@gmail.com>
2023-03-20 21:38:40 +0530

    added entity modification

--- src/main/java/com/practice/entity/Employee.java
+++ src/main/java/com/practice/entity/Employee.java
@@ -5,17 +5,26 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Data
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String password;
-	
+	//hhhh
 	
 
 }
